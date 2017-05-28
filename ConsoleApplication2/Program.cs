6635bafd61b19291b5d5b8ab52ad074af60667a5
chine <chine@DESKTOP-LB1IM66>
2017-05-28 18:15:07 +0200

    new

--- ConsoleApplication2/Program.cs
+++ ConsoleApplication2/Program.cs
@@ -10,10 +10,43 @@ namespace ConsoleApplication2
     {
         static void Main(string[] args)
         {
-           
-            Console.WriteLine("hello");
+            double year, month, day, mnt, scd;
+            DateTime myage;
+            Console.WriteLine("please enter ur name");
+            string name = Console.ReadLine();
+            Console.WriteLine("enter ur password");
+            string pass = Console.ReadLine();
+            Console.WriteLine(" please enter your date");
+            myage = Convert.ToDateTime(Console.ReadLine());
+            year =Convert.ToDouble( DateTime.Now.Subtract(myage).TotalDays / 360);
+            month = Convert.ToDouble(DateTime.Now.Subtract(myage).TotalDays /30 );
+            day = Convert.ToDouble(DateTime.Now.Subtract(myage).TotalHours / 24);
+            mnt = Convert.ToDouble(DateTime.Now.Subtract(myage).TotalMinutes / 60);
+            scd = Convert.ToDouble(DateTime.Now.Subtract(myage).TotalSeconds / 60);
+            Console.WriteLine("your age is ");
+            Console.WriteLine(year + "year");
+            Console.WriteLine(month + "month");
+            Console.WriteLine(day + "day");
+            Console.WriteLine(mnt + "minutes");
+            Console.WriteLine(scd + "seconds");
+            if ((name == "amir") && (pass == "1212"))
+            {
+
+                Console.WriteLine("welcome mr." + name);
+            }
+            else if(!(name == "amir"))
+            {
+                Console.WriteLine("sorry the username is not correct");
+            }
+            else
+            {
+                Console.WriteLine("sorry the password is not correct");
+            }
+            Console.WriteLine("bye..bye");
             Console.ReadKey();
-            
+
+
+
         }
     }
 }
